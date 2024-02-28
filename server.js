0072179dc8cfc594d2b1a7c956b691b01f419211
e6m9 <alostsound@gmail.com>
2024-02-28 16:26:56 -0500

    removed express

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 // import and require packages
-const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'BlackMoon',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+);
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("connected to MySql");
+});
+
 
 // set inquirer questions as array variables for each table
-const homeQuestions = [
-  'what would you like to do?'
-];
-
 const deptQuestions = [
   'please enter the name of the department...'
 ];
@@ -38,33 +45,12 @@ const empRoleUpdate = [
   'please select a new role for the employee...'
 ]
 
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    user: 'root',
-    password: 'BlackMoon',
-    database: 'employees_db'
-  },
-  console.log(`Connected to the employee_db database.`)
-);
-
-// db.connect((err) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log("connected to MySql");
-// });
-
-// app.get('/', async (req, res) => {
-//   const answers = await inquirer.prompt(homeQuestions)
-
 // set up startup view
 app.get('/', (req, res) => {
   const answer = inquirer.prompt(homeQuestions) ([
     {
       type: 'list',
-      message: homeQuestions[0],
+      message: 'what would you like to do?',
       name: 'homeOptions',
       choices:
       [
@@ -86,45 +72,29 @@ app.get('/', (req, res) => {
 });
 
 // set up database view for departments
-app.get('api/departments', (req, res) => {
-
-});
+function viewDepartments() {
+};
 
 // set up database view for roles
-app.get('api/roles', (req, res) => {
-
-});
+function viewRoles() {
+};
 
 // set up database view for employees
-app.get('api/employees', (req, res) => {
-  
-});
+function viewEmployees() {
+};
 
 // set up route for adding a department
-app.post('api/add/department', (req, res) => {
-
-});
+function addDepartment() {
+};
 
 // set up route for adding a role
-app.post('api/add/role', (req, res) => {
-
-});
+function addRole() {
+};
 
 // set up route for adding an employee
-app.post('api/add/employee', (req, res) => {
-
-});
+function addEmployee() {
+};
 
 // set up route for updating an emoployee rold
-app.put('api/update/employeerole', (req, res) => {
-
-});
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+function updateEmployee() {
+};
